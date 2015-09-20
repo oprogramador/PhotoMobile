3626784b39a4767393e685e9a9bc72ed333041f8
oprogramador <uomodislesia@gmail.com>
2015-09-21 00:19:53 +0200

    camera

--- platforms/android/assets/www/js/index.ml.js
+++ platforms/android/assets/www/js/index.ml.js
@@ -1,4 +1,18 @@
-(function() {var bindEvents;bindEvents = (function () {return      eval('$')('#photoButton').click((function () {return          alert('photoABC')
+(function() {var takePhoto;takePhoto = (function () {
+    var onSuccess;onSuccess = (function () {
+        var imageData;imageData = arguments[0];
+        var image;image = document.getElementById('myImage'); return          image.src() = "data:image/jpeg;base64," .__add( imageData
+    )});
+
+    var onFail;onFail = (function () {
+        var message;message = arguments[0]; return          alert('Failed because: ' .__add( message))
+    }); return       navigator['camera'].getPicture(onSuccess, onFail, new AssocArray([
+        'quality', (50),
+        'destinationType', Camera['DestinationType']['DATA_URL']
+    ]))
+});
+
+var bindEvents;bindEvents = (function () {return      eval('$')('#photoButton').click((function () {return          takePhoto()
     }))
 }); return   bindEvents()
 })()
--- platforms/browser/www/js/index.ml
+++ platforms/browser/www/js/index.ml
@@ -1,6 +1,24 @@
+$takePhoto = {
+    $onSuccess = {
+        $imageData = this;
+        $image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData
+    };
+
+    $onFail = {
+        $message = this;
+        alert('Failed because: ' + message)
+    };
+
+    navigator['camera'].getPicture(onSuccess, onFail, %(
+        'quality': 50,
+        'destinationType': Camera['DestinationType']['DATA_URL']
+    ))
+};
+
 $bindEvents = {
     eval('$')('#photoButton').click({
-        alert('photoABC')
+        takePhoto()
     })
 };
 
