756d3a939f745b653ea3f7033513569504e6a73f
oprogramador <uomodislesia@gmail.com>
2015-09-22 00:21:03 +0200

    sqlite

--- platforms/android/assets/www/js/index.ml.js
+++ platforms/android/assets/www/js/index.ml.js
@@ -1,7 +1,10 @@
 (function() {var takePhoto;takePhoto = (function () {
     var onSuccess;onSuccess = (function () {
         var image;image = document.getElementById('myImage');
-        var imageData;imageData = arguments[0]; return          image['src'] = "data:image/jpeg;base64," .__add( imageData
+        var imageData;imageData = arguments[0];
+        var photos;photos = localStorage['photos'].fromJson();
+        photos[(20).randStr()] = imageData;
+        localStorage['photos'] = photos.jsonStr(); return          image['src'] = "data:image/jpeg;base64," .__add( imageData
     )});
 
     var onFail;onFail = (function () {
@@ -12,7 +15,19 @@
     ]))
 });
 
+var drawPhotos;drawPhotos = (function () {
+    (function () {return !localStorage.has('photos')}).if((function () {return          localStorage['photos'] = new AssocArray([]).jsonStr()
+    }));
+    var photos;photos = localStorage['photos'].fromJson();
+    console.log('photos='.__add(photos.keys())); return      photos.keys().each((function () {return          eval('$')('#photos').append('<img src="data:image/jpeg;base64,'.__add(photos[arguments[0]]).__add('"/>'))
+    }))
+});
+
 var bindEvents;bindEvents = (function () {return      eval('$')('#photoButton').click((function () {return          takePhoto()
     }))
-}); return   bindEvents()
+});
+
+var init;init = (function () {
+    bindEvents(); return      drawPhotos()
+}); return   init()
 })()
