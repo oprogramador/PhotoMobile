55d457d1a4f872e9b4fcde937661c1f74f995200
oprogramador <uomodislesia@gmail.com>
2015-09-21 23:17:39 +0200

    localStorage

--- platforms/browser/www/js/index.ml
+++ platforms/browser/www/js/index.ml
@@ -2,6 +2,9 @@ $takePhoto = {
     $onSuccess = {
         $image = document.getElementById('myImage');
         $imageData = this;
+        $photos = localStorage['photos'].fromJson;
+        photos[20.randStr] = imageData;
+        localStorage['photos'] = photos.jsonStr;
         image['src'] = "data:image/jpeg;base64," + imageData
     };
 
@@ -16,10 +19,26 @@ $takePhoto = {
     ))
 };
 
+$drawPhotos = {
+    {!localStorage.has('photos')}.if({
+        localStorage['photos'] = %().jsonStr
+    });
+    $photos = localStorage['photos'].fromJson;
+    console.log('photos='+photos.keys);
+    photos.keys.each({
+        eval('$')('#photos').append('<img src="data:image/jpeg;base64,'+photos[this]+'"/>')
+    })
+};
+
 $bindEvents = {
     eval('$')('#photoButton').click({
         takePhoto()
     })
 };
 
-bindEvents()
+$init = {
+    bindEvents();
+    drawPhotos()
+};
+
+init()
