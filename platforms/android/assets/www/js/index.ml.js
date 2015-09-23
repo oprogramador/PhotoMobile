d0396ae5bfd6a881c23b5f39536c0592951b2a9a
oprogramador <uomodislesia@gmail.com>
2015-09-23 09:17:35 +0200

    android build

--- platforms/android/assets/www/js/index.ml.js
+++ platforms/android/assets/www/js/index.ml.js
@@ -1,11 +1,7 @@
 (function() {var takePhoto;takePhoto = (function () {
     var onSuccess;onSuccess = (function () {
-        var image;image = document.getElementById('myImage');
-        var imageData;imageData = arguments[0];
-        var photos;photos = localStorage['photos'].fromJson();
-        photos[(20).randStr()] = imageData;
-        localStorage['photos'] = photos.jsonStr(); return          image['src'] = "data:image/jpeg;base64," .__add( imageData
-    )});
+        appendPhotoToDraw(arguments[0]); return          savePhoto(arguments[0])
+    });
 
     var onFail;onFail = (function () {
         var message;message = arguments[0]; return          alert('Failed because: ' .__add( message))
@@ -15,11 +11,21 @@
     ]))
 });
 
-var drawPhotos;drawPhotos = (function () {
-    (function () {return !localStorage.has('photos')}).if((function () {return          localStorage['photos'] = new AssocArray([]).jsonStr()
-    }));
+var savePhoto;savePhoto = (function () {
     var photos;photos = localStorage['photos'].fromJson();
-    console.log('photos='.__add(photos.keys())); return      photos.keys().each((function () {return          eval('$')('#photos').append('<img src="data:image/jpeg;base64,'.__add(photos[arguments[0]]).__add('"/>'))
+    photos[(20).randStr()] = arguments[0]; return      localStorage['photos'] = photos.jsonStr()
+});
+
+var appendPhotoToDraw;appendPhotoToDraw = (function () {return      eval('$')('#photos').append('<img src="data:image/jpeg;base64,'.__add(arguments[0]).__add('"/>'))
+});
+
+var getAllPhotos;getAllPhotos = (function () {
+    (function () {return !localStorage.has('photos')}).if((function () {return          localStorage['photos'] = new AssocArray([]).jsonStr()
+    })); return      localStorage['photos'].fromJson()
+});
+
+var drawPhotos;drawPhotos = (function () {
+    var photos;photos = getAllPhotos(); return      photos.keys().each((function () {return          appendPhotoToDraw(photos[arguments[0]])
     }))
 });
 
