6d5173a457fc6faa6e7813a58e67eb88bf007c60
oprogramador <uomodislesia@gmail.com>
2016-09-07 00:55:35 +0200

    remove plugins from repo

---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+set -e
+cordova platform add android
+cordova platform add ios
+cordova platform add browser
+cordova plugin add cordova-plugin-camera
+cordova plugin add cordova-sqlite-storage
+cordova plugin add cordova-plugin-file
